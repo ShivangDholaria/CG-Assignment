1a06513320e137a469125eb5958f62d6840f32ab
Shivang Dholaria <dholariashivang@gmail.com>
2021-07-30 14:40:16 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,212 @@
+#include <GL/glut.h>
+#include <stdio.h>
+using namespace std;
+
+#define FROM_RIGHT      1
+#define FROM_LEFT       2
+#define FROM_TOP        3
+#define FROM_BOTTOM     4
+
+static int WINDOW_WIDTH ,WINDOW_HEIGHT;
+
+int playerResult=0;
+static float Xspeed=.5,Yspeed=.5;
+static float delta=.31;
+
+char string [100];
+
+struct RECTA
+{
+        float left,top,right,bottom;
+};
+
+RECTA ball={100,100,120,120};
+RECTA wall ;
+RECTA player_1 ={0,490, 60,500};
+
+void DrawRectangle(RECTA  rect)
+{
+        glBegin(GL_QUADS);
+            glVertex2f(rect.left,rect.bottom);
+            glVertex2f(rect.right,rect.bottom);
+            glVertex2f(rect.right,rect.top);
+            glVertex2f(rect.left,rect.top);
+        glEnd();
+}
+
+void Timer(int v)
+{
+        ball.left+=Xspeed;
+        ball.right+=Xspeed;
+        ball.top+=Yspeed;
+        ball.bottom+=Yspeed;
+
+        glutTimerFunc(1,Timer,1);
+}
+
+void drawText(char*string,int x,int y)
+{
+	  char *c;
+	  glPushMatrix();
+	  glTranslatef(x, y,0);
+	  glScalef(0.1,-0.1,1);
+
+	  for (c=string; *c != '\0'; c++)
+	  {
+    		glutStrokeCharacter(GLUT_STROKE_ROMAN , *c);
+	  }
+	  glPopMatrix();
+
+}
+
+void stop()
+{
+   printf("game over");
+    exit(0);
+}
+
+//Ball wall collision test
+int Test_Ball_Wall(RECTA  ball , RECTA wall)
+{
+        if(ball.right >=wall.right)
+            return FROM_RIGHT;
+
+        if(ball.left <=wall.left)
+            return FROM_LEFT;
+
+        if(ball.top <=wall.top)
+            return FROM_TOP;
+
+        if(ball.bottom >=wall.bottom)
+            stop();
+
+        else return 0 ;
+}
+
+//Ball player collision test
+bool Test_Ball_Player(RECTA ball,RECTA player)
+{
+
+		if(ball.bottom >= player.top && ball.left>= player.left && ball.right <=player.right )
+                {
+			playerResult++;
+
+                  return true;
+		}
+
+            return false;
+}
+
+
+void keyboard(unsigned char key, int x, int y)
+{
+   switch (key) {
+      case 27:
+         exit(0);
+         break;
+   }
+}
+
+static int mouse_x=0;
+
+void MouseMotion(int x,int y)
+{
+	mouse_x=x;
+}
+
+
+// OpenGL Setting
+void Setting(void)
+{
+   glClearColor (0.0, 0.0, 0.0, 0.0);
+   glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+}
+
+
+void reshape (int w, int h)
+{
+	WINDOW_WIDTH =w ;
+	WINDOW_HEIGHT =h ;
+
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+
+   glMatrixMode (GL_PROJECTION);
+   glLoadIdentity ();
+   gluOrtho2D (0, w, h, 0);
+
+
+   glMatrixMode (GL_MODELVIEW);
+   glLoadIdentity ();
+
+}
+
+int pcResult =0;
+
+void Render()
+{
+	glClear(GL_COLOR_BUFFER_BIT );
+	glLoadIdentity();
+
+	sprintf(string,"PC : %d ",pcResult);
+	drawText(string,10,80);
+	sprintf(string,"Player : %d ",playerResult);
+	drawText(string,10,120);
+
+	wall.left=wall.top=0;
+	wall.right=WINDOW_WIDTH;
+	wall.bottom=WINDOW_HEIGHT;
+
+	glColor3f(.4, .7, .1);
+	DrawRectangle(ball);
+
+	if(Test_Ball_Wall(ball,wall)== FROM_RIGHT)
+    Xspeed=-delta;
+
+    if(Test_Ball_Wall(ball,wall)== FROM_LEFT)
+    Xspeed=delta;
+
+    if(Test_Ball_Wall(ball,wall)== FROM_TOP)
+    Yspeed=delta;
+
+    if(Test_Ball_Wall(ball,wall)== FROM_BOTTOM)
+	{
+		Yspeed=-delta;
+		pcResult +=1;
+	}
+
+	glColor3f(.6, .3, .4);
+	DrawRectangle(player_1);
+    player_1.left=mouse_x-20;
+    player_1.right=mouse_x+60;
+
+
+    if(Test_Ball_Player(ball,player_1)==true)
+            Yspeed=-delta;
+
+	glutSwapBuffers();
+}
+
+
+int main(int argc, char** argv)
+{
+   glutInit(&argc, argv);
+   glutInitDisplayMode ( GLUT_DOUBLE | GLUT_RGB);
+   glutInitWindowSize (795, 500);
+   glutInitWindowPosition (0, 0);
+   glutCreateWindow ("GAME");
+
+   Setting ();
+
+   glutDisplayFunc(Render);
+   glutIdleFunc(Render);
+   glutTimerFunc(1,Timer,1);
+
+   glutReshapeFunc(reshape);
+   glutKeyboardFunc(keyboard);
+
+   glutPassiveMotionFunc(MouseMotion);
+   glutMainLoop();
+
+   return 0;
+}
